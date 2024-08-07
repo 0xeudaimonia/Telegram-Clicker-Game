1676f2da20489d547cdb0022277ac8d8a86b1513
0xeudaimonia <selectthebest18@gmail.com>
2024-08-07 21:02:05 +0000

    fix next.config.mjs conflict

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,10 @@
 const nextConfig = {
   // reactStrictMode: true,
   // swcMinify: true
-<<<<<<< HEAD
   env: {
     TELGRAM_BOT_LINK: process.env.NEXT_PUBLIC_TELGRAM_BOT_LINK,
     TELEGRAM_SHARE_LINK: process.env.NEXT_PUBLIC_TELEGRAM_SHARE_LINK
   },
-=======
->>>>>>> b525c95bc3441d1d960e0c44667389b5b4bd6f5b
 };
 
 export default nextConfig;
